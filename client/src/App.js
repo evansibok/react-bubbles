3cc0d866ad8f348e319b80acfc919c1508663fcb
Evans Ibok <evansibok@gmail.com>
2019-12-20 09:32:24 +0100

    create PrivateRoute wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -18,4 +18,10 @@ function App() {
   );
 }
 
+const PrivateRoute = ({ component: Component, props }) => (
+  <Route
+    {...props}
+    render={props => localStorage.getItem("token") ? (<Component {...props} />) : (<Redirect to="/" />)}
+  />
+)
 export default App;
