a1751df85afb9587ebe2c4d99dc3e23aa07fbb9a
Evans Ibok <evansibok@gmail.com>
2019-12-20 09:18:40 +0100

    create login form and manage state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Login = () => {
+  const initialForm = {
+    username: "",
+    password: "",
+  }
+  const [loginForm, setLoginForm] = useState(initialForm);
+
+  const inputChange = evt => {
+    const { name, value } = evt.target;
+    setLoginForm({
+      ...loginForm,
+      [name]: value,
+    })
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            value={loginForm.username}
+            onChange={inputChange} />
+        </label>
+
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            value={loginForm.password}
+            onChange={inputChange} />
+        </label>
+        <button>Login</button>
+      </form>
     </>
   );
 };
